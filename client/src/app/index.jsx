bc07ec497983b90bddc8ee5e3ad65accfe8f4089
Aleksandr <Avshef451987@gmail.com>
2024-01-21 22:07:33 +0500

    [development/initialize project]: added an example of a component, using css-bem, and documenting using JSDoc

---
+++ client/src/app/index.jsx
@@ -0,0 +1,10 @@
+import Counter from "../components/counter-example/index";
+function App() {
+    return (
+        <>
+            <Counter initialCount={0} />
+        </>
+    );
+}
+
+export default App;
---
+++ client/src/components/counter-example/index.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import "./style.css";
+/**
+ * Functional component representing a simple counter.
+ *
+ * @component
+ * @example
+ * // Example usage of Counter component
+ * <Counter initialCount={0} />
+ *
+ * @param {Object} props - React component props.
+ * @param {number} props.initialCount - Initial count value.
+ * @returns {JSX.Element} - Rendered Counter component.
+ */
+function Counter({ initialCount }) {
+    const [count, setCount] = useState(initialCount);
+
+    /**
+     * Callbacks for handling counter operations.
+     *
+     * @namespace
+     * @property {Function} handleIncrement - Increments the counter.
+     * @property {Function} handleDecrement - Decrements the counter.
+     */
+    const callbacks = {
+        handleIncrement() {
+            setCount((prevCount) => prevCount + 1);
+        },
+        handleDecrement() {
+            setCount((prevCount) => prevCount - 1);
+        },
+    };
+
+    return (
+        <div className="Counter">
+            <p className="Counter__text">Count: {count}</p>
+            <button
+                className="Counter__text"
+                onClick={callbacks.handleIncrement}
+            >
+                Increment
+            </button>
+            <button
+                className="Counter__button Counter__button-disabled"
+                onClick={callbacks.handleDecrement}
+            >
+                Decrement
+            </button>
+        </div>
+    );
+}
+
+export default Counter;
